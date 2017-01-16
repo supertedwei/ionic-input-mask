e2c1778b8879f44e44e409f343d8aec7a172a314
Ted Wei <jdwei@hotmail.com>
2017-01-16 11:18:53 +0800

    Add TextMaskModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { TextMaskModule } from 'angular2-text-mask';
+ 
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    TextMaskModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
