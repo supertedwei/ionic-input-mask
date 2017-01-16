2dd87bb6300b61869b63c74aea87566335d8dee7
Ted Wei <jdwei@hotmail.com>
2017-01-16 11:19:21 +0800

    Create a Form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
-
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-    
-  }
-
+ 
+    masks: any;
+ 
+    phoneNumber: any = "";
+    cardNumber: any = "";
+    cardExpiry: any = "";
+    orderCode: any = "";
+ 
+    constructor(public navCtrl: NavController) {
+ 
+    }
+ 
+    save(){
+ 
+    }
+ 
 }
